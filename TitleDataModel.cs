d79a57692bf7904f0a660a54104159b9d25c3d95
Mikael Olsson <pmolsson@live.com>
2025-02-05 15:48:01 +0100

    fix

--- TitleDataModel.cs
+++ TitleDataModel.cs
@@ -13,18 +13,3 @@ namespace SqLiteAppNetCoreV2
     }
 
 }
-
-using System;
-
-
-namespace SqLiteAppNetCoreV2
-{
-    public class TitleDataModel
-    {        
-        public string Name { get; set; }
-        public string Parent { get; set; }
-        public long Size { get; set; }
-
-    }
-
-}
